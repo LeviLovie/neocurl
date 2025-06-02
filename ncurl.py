39fc5e00aeebc06281ea972bb76544412a3bcab0
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2025-06-02 12:14:48 +0700

    Prepare 2.0.0

--- ncurl.py
+++ ncurl.py
@@ -4,7 +4,7 @@ import neocurl as nc
 
 @nc.on_init
 def main():
-    if not nc.check_version("2.0.0-rc.2"):
+    if not nc.check_version("2.0.0"):
         nc.fatal(f"This version of neocurl is not compatible with this script: {nc.version()}")
 
     logger_config = nc.get_logger_config()
